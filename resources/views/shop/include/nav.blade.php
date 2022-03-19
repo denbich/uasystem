9518d4b65fccb0faeda6423ffdfec8b6e58e8f93
denbich <denis.bichler9@gmail.com>
2022-03-19 23:17:33 +0100

    Update nav.blade.php

--- resources/views/shop/include/nav.blade.php
+++ resources/views/shop/include/nav.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-top navbar-expand navbar-dark bg-primary border-bottom">
     <div class="container-fluid">
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main" method="GET" action="">
+        <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main" method="GET" action="{{ route('s.ukrainian.search') }}">
           <div class="form-group mb-0">
             <div class="input-group input-group-alternative input-group-merge">
               <div class="input-group-prepend">
                 <span class="input-group-text"><i class="fas fa-search"></i></span>
               </div>
-              <input class="form-control" placeholder="Szukaj (szukanie niedostępne)" type="text">
+              <input class="form-control" placeholder="Szukaj uchodźca" name="q" type="text">
             </div>
           </div>
           <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main" aria-label="Close">
