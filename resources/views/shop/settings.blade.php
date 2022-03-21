4704b1d5ab6895fa4c2d5ce93b206278bb339758
denbich <denis.bichler9@gmail.com>
2022-03-21 20:29:42 +0100

    docs

--- resources/views/shop/settings.blade.php
+++ resources/views/shop/settings.blade.php
@@ -47,6 +47,13 @@
             </li>
 
             <li class="nav-item">
+                <a class="nav-link" href="https://docs.google.com/spreadsheets/d/1SYVe6dfqVk0SOJyWWnXtlLcQJ3WMcv1rhHX8WuOMiYo/edit?usp=sharing" target="_blank">
+                    <i class="fas fa-table text-primary"></i>
+                    <span class="nav-link-text">Grafik dużurów</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
                 <a class="nav-link" href="" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     <i class="fas fa-sign-out-alt text-primary"></i>
                     <span class="nav-link-text">Wyloguj się</span>
