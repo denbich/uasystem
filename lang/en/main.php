aa3e7037f7aef4a857ebac3aa9c86307b33d52d0
denbich <denis.bichler9@gmail.com>
2022-03-22 12:33:53 +0100

    Update main.php

--- lang/en/main.php
+++ lang/en/main.php
@@ -8,16 +8,22 @@ return [
     'signin' => 'Sign in',
     'lang' => 'Choose language',
     'morelang' => 'More languages...',
-    'success' => 'Sukces',
+    'success' => 'Success',
     'cancel' => 'Cancel',
+    'confirm' => 'Confirm',
+    'save' => 'Save',
     'error' => 'Error',
     'alert' => 'Alert',
+    'addrefugee' => 'Add refugee',
+    'refugee' => 'Refugees',
+    'options' => 'Options',
+    'null' => 'Lack',
 
     'langlist' => [
         'current' => [
-            'polish' => 'Polish',
-            'english' => 'English',
-            'ukrainian' => 'Ukrainian',
+            'polish' => 'Polski',
+            'english' => 'Angielski',
+            'ukrainian' => 'Ukraiński',
         ],
 
         'foreign' => [
@@ -25,6 +31,17 @@ return [
             'english' => 'English',
             'ukrainian' => 'Yкраїнський',
         ],
+    ],
+
+    'menu' => [
+        'mainpage' => 'Main page',
+    ],
+
+    'auth' => [
+        'panel' => 'uaSystem Panel',
+        'login' => 'Username',
+        'password' => 'Password',
+        'rememberme' => 'Remember me',
     ],
 ];
 
