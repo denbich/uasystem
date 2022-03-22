40405c20328109ab8494d49a2ff5907a475693f8
denbich <denis.bichler9@gmail.com>
2022-03-22 16:12:29 +0100

    Update edit.blade.php

--- resources/views/shop/ukrainian/edit.blade.php
+++ resources/views/shop/ukrainian/edit.blade.php
@@ -75,7 +75,7 @@
                 <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                   <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                     <li class="breadcrumb-item"><a href="{{ route('s.dashboard') }}"><i class="fas fa-home"></i></a></li>
-                    <li class="breadcrumb-item active" aria-current="page">{{ __('shop.sidemenu.general.refugees') }}</li>
+                    <li class="breadcrumb-item active" aria-current="page">{{ __('shop.sidemenu.general.refugees.title') }}</li>
                     <li class="breadcrumb-item">{{ __('shop.refugees.edit.edit') }}</li>
                   </ol>
                 </nav>
