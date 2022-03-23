7f0ff67ea515a75a28c2ed3f7f5383ab5e0d2ad2
denbich <denis.bichler9@gmail.com>
2022-03-23 16:53:17 +0100

    Update dashboard.blade.php

--- resources/views/shop/dashboard.blade.php
+++ resources/views/shop/dashboard.blade.php
@@ -88,7 +88,7 @@
                     <div class="row">
                       <div class="col">
                         <h5 class="card-title text-uppercase text-muted mb-0">{{ __('shop.dashboard.todaycount') }}</h5>
-                        <span class="h2 font-weight-bold mb-0">{{ $chart[1]['old'] }}</span>
+                        <span class="h2 font-weight-bold mb-0">{{ $visits_today }}</span>
                       </div>
                       <div class="col-auto">
                         <div class="icon icon-shape bg-success text-white rounded-circle shadow">
