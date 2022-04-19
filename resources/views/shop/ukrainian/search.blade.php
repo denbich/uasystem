0ddce63700e558e0595a8cde09fd70d912746098
denbich <denis.bichler9@gmail.com>
2022-04-19 17:26:40 +0200

    as

--- resources/views/shop/ukrainian/search.blade.php
+++ resources/views/shop/ukrainian/search.blade.php
@@ -236,7 +236,7 @@
                         <label class="custom-control-label" for="Check1{{ $uk->id }}">{{ __('shop.refugees.list.clothes') }}</label>
                       </div>
                       <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="Check2{{ $uk->id }}" name="food">
+                        <input type="checkbox" class="custom-control-input" id="Check2{{ $uk->id }}" name="food"  checked>
                         <label class="custom-control-label" for="Check2{{ $uk->id }}">{{ __('shop.refugees.list.food') }}</label>
                       </div>
                       <div class="custom-control custom-checkbox">
